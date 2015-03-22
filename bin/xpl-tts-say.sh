f3f06dfd7b1602385ef158ff85c581c8557b651b
pcarlier <philippe.carlier.fr@gmail.com>
2015-03-22 23:17:43 +0100

    add voice parameter

--- bin/xpl-tts-say.sh
+++ bin/xpl-tts-say.sh
@@ -1,3 +1,21 @@
 #! /bin/bash
-echo "TTS : $1"
-xpl-sender -m xpl-cmnd -c tts.basic speech="$1"
+
+DEFVOICE="google"
+
+if test  "$#" -lt 1; then
+    echo "xpl-tts-say.sh [message] [voice]"
+    echo "voice: google, agnes, loic, papi, electra, robot, sorciere, melodine, ramboo, chut, yeti, bicool"
+    echo " " 
+    echo " samples:"
+    echo "    xpl-tts-say.sh \"bonjour, test de message\"  google"
+    echo "    xpl-tts-say.sh \"bonjour, test de message\"  yeti"
+    exit -1
+fi
+
+MESSAGE=$1
+VOICE=${2:$DEFVOICE}
+
+echo "MESSAGE : $MESSAGE"
+echo "VOICE   : $VOICE"
+
+xpl-sender -m xpl-cmnd -c tts.basic voice="$VOICE" speech="$MESSAGE" 
